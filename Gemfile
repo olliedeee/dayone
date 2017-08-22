8ee82562a1b851d8397f2aa19c425e5686a77c3e
Ollie <oliverbucks@gmail.com>
2017-08-22 21:31:21 +0000

    devise branch

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'carrierwave', '0.11.2'
 gem 'mini_magick'
 gem 'fog-aws'
 gem 'puma', '~> 3.10'
+gem 'devise'
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  
-  root "pages#home"
-  get "pages/home", to: "pages#home"
-  
-  
-  resources :goals do
-    resources :comments, only: [:create]
-  end
-  
-  resources :users, except: [:new]
-  resources :categories, except: [:destroy]
-  
-  get "/signup", to: "users#new"
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
-
-  mount ActionCable.server => '/cable'
-  
 end
