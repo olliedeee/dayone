515881f172d9ccba907ce89d5d8d3125e94af9f6
Ollie <oliverbucks@gmail.com>
2017-08-25 21:00:13 +0000

    passing tests, pre-role add to user table

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'User index page', :devise do
+  scenario 'user sees own username' do
+    user = FactoryGirl.create(:user)
+    login_as(user, scope: :user)
+    visit users_path
+    expect(page).to have_content user.username
+  end
+end
